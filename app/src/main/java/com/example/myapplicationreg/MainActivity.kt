30496e6bcf7ff58f0a3664424f62536dfa988722
CuteDino <rubin4ik5553535@gmail.com>
2020-12-23 19:13:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationreg/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myapplicationreg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    var login = "123"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var inputLogin = findViewById<EditText>(R.id.editTextTextPersonName2)
+        findViewById<Button>(R.id.button).setOnClickListener(){
+            if(login == inputLogin.text.toString()){
+
+            }
+            else{
+                Toast.makeText(this, "Неправильный логин", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
